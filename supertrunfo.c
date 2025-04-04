27a433d574e0eac3bb55062351122e85045416a4
KainanTamer <202502108141@alunos.estacio.br>
2025-04-04 12:00:04 +0000

    terminei o projeto

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,6 +3,14 @@
 int main(){
 
 printf("Desafio Super Trunfo\n");
+printf("Carta 1\n");
+char estado
+char codigo 
+char nome da cidade
+int populaçao
+float Area 
+float PIB
+int Numero de pontos turisticos
 
 
 }
