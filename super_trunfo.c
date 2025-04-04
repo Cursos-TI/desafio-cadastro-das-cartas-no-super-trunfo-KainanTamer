e1d7cc6f5cd75556cd090584bdc760abb7cc000e
KainanTamer <202502108141@alunos.estacio.br>
2025-04-04 12:21:34 +0000

    projeto terminado

---
+++ super_trunfo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+ int main (){
+
+    printf("Desafio do Super Trunfo\n");
+    printf("Carta 1\n");
+    char estado1 = 'A';
+    char codigo1[20] = "A01";
+    char nome_da_cidade1[20] = "Manaus";
+    int populacao1 = 2435000;
+    float Area1 = 11401;
+    float PIB1 = 103281000000;
+    int Numero_de_pontos_turisticos1 = 40;
+ 
+    printf("O estado é: %c\n", estado1); 
+    printf("O codigo é: %s\n", codigo1);
+    printf("O nome da cidade é: %s\n", nome_da_cidade1); 
+    printf("O numero da populaçao é: %d\n", populacao1);
+    printf("A area da cidade é: %f\n", Area1);
+    printf("O PIB da cidade é: %f\n", PIB1);
+    printf("O numero de pontos turisticos é: %d\n",Numero_de_pontos_turisticos1);
+ 
+   printf("Carta 2\n");
+    char estado2 = 'B';
+    char codigo2[20] = "B02";
+    char nome_da_cidade2[20] = "Rio de Janeiro";
+    int populacao2 = 6748000;
+    float Area2 = 1200.25;
+    float PIB2 = 300050000000;
+    int Numero_de_pontos_turisticos2 = 30;
+ 
+    printf("O estado é: %c\n", estado2); 
+    printf("O codigo é: %s\n", codigo2);
+    printf("O nome da cidade é: %s\n", nome_da_cidade2); 
+    printf("O numero da populaçao é: %d\n", populacao2);
+    printf("A area da cidade é: %f\n", Area2);
+    printf("O PIB da cidade é: %f\n", PIB2);
+    printf("O numero de pontos turisticos é: %d\n",Numero_de_pontos_turisticos2);
+ 
+    return 0;
+ }
